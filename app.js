2246a453ed405ac0178796a0c17189e2d136d74f
Nazmul Hossain <nazhossain16@gmail.com>
2024-01-22 14:17:13 -0500

    Version 3

---
+++ app.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper(".swiper", {
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+let sections = document.querySelectorAll("section");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + height) {
+      sec.classList.add("show-animate");
+    } else {
+      sec.classList.remove("show-animate");
+    }
+  });
+};
